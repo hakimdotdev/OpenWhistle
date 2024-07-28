cca0bb094ae291447aae68572ee2a7891348e92e
hakimdotdev <hakim@liso.email>
2024-07-28 21:12:37 +0200

    add empty constructor for ef core

--- Models/FollowUpAction.cs
+++ Models/FollowUpAction.cs
@@ -1,7 +1,10 @@
+using OpenWhistle.Models.Common;
+
 namespace OpenWhistle.Models;
 
 public class FollowUpAction : BaseEntity
 {
+    private FollowUpAction(){}
     public string ActionDescription { get; set; }
     public WhistleblowerReport Report { get; set; }
 }
